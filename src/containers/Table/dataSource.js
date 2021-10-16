6e6dba79eb9fa61f467b5f56325c6a6d53cf87b9
ceferovagulbahar4 <ceferovagulbahar4@gmail.com>
2021-10-16 18:20:18 +0400

    Parent data view

--- src/containers/Table/dataSource.js
+++ src/containers/Table/dataSource.js
@@ -1,12 +1,26 @@
 import LocalStore from "devextreme/data/local_store"
 import DataSource from "devextreme/data/data_source"
 
+
 const store = new LocalStore({
-  key: "id",
+  key: "id" ,
   name: "tree-list-local-data",
   immediate: true,
+  onInserted: (value, key)=>{
+    if( window.localStorage )
+    {
+      if(!localStorage.getItem('firstLoad'))
+      {
+        localStorage['firstLoad'] = true;
+        window.location.reload();
+      }  
+      else
+        localStorage.removeItem('firstLoad');
+    }
+  },
 })
 
 export const dataSource = new DataSource({
   store,
 })
+
